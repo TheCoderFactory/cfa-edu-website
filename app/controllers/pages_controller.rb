035689248a50cb27f8d8d840bdaf4258ffa73e13
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-05-24 16:21:05 +1000

    other basic pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,28 @@
 class PagesController < ApplicationController
+  def about
+  end
+
+  def alumni
+  end
+
+  def career_outcomes
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def information_toolkit
+  end
+
+  def instructors
+  end
+
+  def privacy
+  end
+
+  def video_archive
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   get "fast-track", to: "fast_track#index"
   get "fast-track/apply", to: "fast_track#apply"
+
+  get "about", to: "pages#about"
+  get "meet-our-alumni", to: "pages#alumni"
+  get "career-outcomes", to: "pages#career_outcomes"
+  get "contact", to: "pages#contact"
+  get "faq", to: "pages#faq"
+  get "information-toolkit", to: "pages#information_toolkit"
+  get "meet-our-instructors", to: "pages#instructors"
+  get "privacy", to: "pages#privacy"
+  get "video-archive", to: "pages#video_archive"
 end
