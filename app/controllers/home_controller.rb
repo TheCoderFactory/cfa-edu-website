6189f1d5ac9cfa7b8c57c6dc4d0fb20bdbb3f940
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-06-23 09:38:03 +1000

    timeline, dynamic intakes for home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @intakes = Intake.all.reverse_chron_order.first(4)
   end
 end
--- app/models/intake.rb
+++ app/models/intake.rb
@@ -10,10 +10,26 @@ class Intake < ActiveRecord::Base
       errors.add(:finish, "finish cannot be before start")
     end
   end
+
+  def self.reverse_chron_order
+    order(start: :desc)
+  end
   def intake_available?
     total_attendees = 0
     bookings.each { |b| total_attendees+=b.people_attending }
     return true if total_attendees < class_size
     return false
+  end
+  def course_price
+    course.get_price
+  end
+  def course_name
+    course.name
+  end
+  def start_date
+    start.strftime('%d %B %Y')
+  end
+  def start_time
+    start.strftime("%I:%M %P").upcase
   end
 end
