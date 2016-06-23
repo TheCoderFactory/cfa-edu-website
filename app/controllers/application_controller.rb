21efa5539bee68aacf512220a85367dc98cd4117
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-06-23 11:49:44 +1000

    new page content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :get_footer_course_lists
+
   def after_sign_in_path_for(resource_or_scope)
     admin_dashboard_path
   end
   def after_sign_out_path_for(resource_or_scope)
     root_path
+  end
+
+  def get_footer_course_lists
+    @footer_short_courses = Course.where(course_type: "Workshop")
+    @footer_corporate_courses = Course.where(course_type: "Corporate")
+    @footer_schools_courses = Course.where(course_type: "Schools")
   end
 end
