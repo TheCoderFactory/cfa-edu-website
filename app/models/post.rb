bb8c1fae66a26364ec20a4ac255496eed716aee8
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-06-24 15:41:32 +1000

    no blog post images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   extend FriendlyId
-  mount_uploader :image, ImageUploader
-  mount_uploader :author_image, AuthorImageUploader
+  # mount_uploader :image, ImageUploader
+  # mount_uploader :author_image, AuthorImageUploader
   friendly_id :title, use: :slugged
 
   validates :title, :lead, :content, :image, :published_date, presence: true
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
-      post = Post.create row.to_hash.except!("user_id")
+      post = Post.create row.to_hash.except!("user_id", "id", "impressions_count")
       post.image = row.to_hash["image"]
       post.save
     end
