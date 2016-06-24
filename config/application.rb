036d22974b6808442e69d6786b9174ed19868be1
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-06-24 11:24:44 +1000

    application.rb for ckeditor

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,6 @@ module CfaEduWebsite
         request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
