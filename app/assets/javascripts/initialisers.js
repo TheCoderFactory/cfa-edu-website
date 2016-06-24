6e74e8861273b8cdcc59d657eabdd32f90e3f323
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-06-24 10:46:02 +1000

    precompile initialisers

---
+++ app/assets/javascripts/initialisers.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('.ckeditor').ckeditor();
+  $('#start-datetimepicker').datetimepicker({
+    format: "DD/MM/YYYY hh:mm A"
+  });
+  $('#finish-datetimepicker').datetimepicker({
+    useCurrent: false, //Important! See issue #1075
+    format: "DD/MM/YYYY hh:mm A"
+  });
+  $("#start-datetimepicker").on("dp.change", function (e) {
+    $('#finish-datetimepicker').data("DateTimePicker").minDate(e.date);
+  });
+  $("#finish-datetimepicker").on("dp.change", function (e) {
+    $('#start-datetimepicker').data("DateTimePicker").maxDate(e.date);
+  });
+});
