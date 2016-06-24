0d3d3779c830f903790c8dc16131f7da424d3760
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-06-24 14:32:57 +1000

    blog sidebar

--- app/assets/javascripts/blog_sidebar.js
+++ app/assets/javascripts/blog_sidebar.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
-  if ($('.promos').length) var offset = $('.promos').offset().top;
-  $.fn.followTo = function (pos) {
-    var $this = this,
-    $window = $(window);
+// $(document).ready(function() {
+  // if ($('.promos').length) var offset = $('.promos').offset().top;
+  // $.fn.followTo = function (pos) {
+    // var $this = this,
+    // $window = $(window);
 
-    $window.scroll(function (e) {
-      if ($window.scrollTop() >= pos) {
-        $this.css({
-          position: 'absolute',
-          top: pos
-        });
-      } else {
-        $this.css({
-          position: 'fixed',
-          top: offset
-        });
-      }
-    });
-  };
-  $('.promos').followTo($('#left-column').outerHeight(true) - $('.promos').outerHeight(true));
-});
+    // $window.scroll(function (e) {
+      // if ($window.scrollTop() >= pos) {
+        // $this.css({
+          // position: 'absolute',
+          // top: pos
+        // });
+      // } else {
+        // $this.css({
+          // position: 'fixed',
+          // top: offset
+        // });
+      // }
+    // });
+  // };
+  // $('.promos').followTo($('#left-column').outerHeight(true) - $('.promos').outerHeight(true));
+// });
