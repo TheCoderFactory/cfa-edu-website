85f06c89ef92b678ccdcdb3a3df3004bb57b5b27
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-06-24 17:28:52 +1000

    bookings logging

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -70,6 +70,8 @@ class BookingsController < ApplicationController
     if @booking.update_attributes(booking_params)
       redirect_to @booking
     else
+      puts @booking.errors.inspect
+      puts @payment.errors.inspect
       respond_with @booking
     end
   end
