93f24903472edf6ef105bebf55866fe8e7eba3c2
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-06-27 17:43:35 +1000

    change spelling

--- app/controllers/kids_coding_controller.rb
+++ app/controllers/kids_coding_controller.rb
@@ -1,6 +1,6 @@
 class KidsCodingController < ApplicationController
   def index
-    @coding_and_the_digital_curriculum = Course.find_by(name: "Coding And The Digital Curriculum")
+    @coding_and_the_digital_curriculum = Course.find_by(name: "Coding and the Digital Curriculum")
     @student_deeper_dive = Course.find_by(name: "Student Deeper Dive")
     @hands_on_coding = Course.find_by(name: "Hands On Coding")
     @courses = Course.where(course_type: "Schools")
