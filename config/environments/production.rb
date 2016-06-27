44edfe6828a167984e9d9aff00049520efbf1ef7
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-06-27 11:28:41 +1000

    email settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'htps://coderfactoryacademy.edu.au' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    :port           => 587,
+    :address        => 'smtp.mailgun.org',
+    :user_name      => ENV['MAILGUN_USER_NAME'],
+    :password       => ENV['MAILGUN_PASSWORD'],
+    :domain         => 'email.coderfactoryacademy.edu.au',
+    :authentication => :plain
+  }
 end
