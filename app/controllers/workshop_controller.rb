01a22c5def20956ed33d4fc1aa1d6cb90c143505
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-06-27 12:24:50 +1000

    update 12 week course to web app builder

--- app/controllers/workshop_controller.rb
+++ app/controllers/workshop_controller.rb
@@ -1,8 +1,8 @@
 class WorkshopController < ApplicationController
   def index
-    @coding_for_beginners = Course.find_by(name: "Coding For Beginners")
-    @twelve_week_course = Course.find_by(name: "12 Week Course")
-    @coding_kickstarter = Course.find_by(name: "Coding Kickstarter")
+    @course1 = Course.find_by(name: "Coding For Beginners")
+    @course2 = Course.find_by(name: "Web App Builder")
+    @course3 = Course.find_by(name: "Coding Kickstarter")
     @courses = Course.where(course_type: "Workshop")
     @intakes = Intake.all.future_intakes.chron_order.first(4)
   end
