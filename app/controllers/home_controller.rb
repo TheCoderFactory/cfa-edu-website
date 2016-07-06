c926e2a36e9c140fc4b92430ef521eaf302edf38
Azzapop <a.b.hook@live.com.au>
2016-07-06 12:43:47 +1000

    only show future intakes in course selection

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @intakes = Intake.all.chron_order.first(4)
+    @intakes = Intake.all.future_intakes.chron_order.first(4)
     @posts = Post.current_articles.first(3)
   end
 end
