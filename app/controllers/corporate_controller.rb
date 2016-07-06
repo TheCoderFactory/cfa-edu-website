817b400ff4ac2a95523527e967f921c1435c2049
Azzapop <a.b.hook@live.com.au>
2016-07-06 20:49:53 +1000

    change demysitfy tech to digital pioneer

--- app/controllers/corporate_controller.rb
+++ app/controllers/corporate_controller.rb
@@ -1,8 +1,8 @@
 class CorporateController < ApplicationController
   def index
-    @demystify_tech = Course.find_by(name: "Demystify Tech")
-    @transformer = Course.find_by(name: "Transformer")
-    @intro_to_coding = Course.find_by(name: "Introduction To Coding")
+    @course1 = Course.find_by(name: "Digital Pioneer")
+    @course2 = Course.find_by(name: "Transformer")
+    @course3 = Course.find_by(name: "Introduction To Coding")
     @courses = Course.where(course_type: "Corporate")
     @posts = Post.current_articles.first(3)
   end
