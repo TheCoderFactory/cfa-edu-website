f2e97ffdbc95d84ee518419cac5c3b353e03740c
Azzapop <a.b.hook@live.com.au>
2016-10-05 10:42:37 +1100

    fix bug on dashboard

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -4,10 +4,10 @@ class AdminDashboardController < ApplicationController
   layout "admin"
 
   def index
-    this_month = Intake.all.reject{ |i| i.start.month != DateTime.today.month }.map {|i| i.course_name+','+i.start_date }
-    next_month = Intake.all.reject{ |i| i.start.month != DateTime.today.month + 1.month }.map {|i| i.course_name+','+i.start_date }
+    this_month = Intake.all.reject{ |i| i.start.month != DateTime.now.month }.map {|i| i.course_name+','+i.start_date }
+    next_month = Intake.all.reject{ |i| i.start.month != DateTime.now.month + 1.month }.map {|i| i.course_name+','+i.start_date }
     @upcoming_intakes = this_month.zip(next_month)
-    @bookings = Booking.where("created_at <= ?", Date.today-30.days)
+    @bookings = Booking.where("created_at >= ?", Date.today-30.days)
     @pie_bookings = @bookings.inject(Hash.new(0)) { |hash, i|  hash[i.course_name]+=1; hash }
   end
 end
