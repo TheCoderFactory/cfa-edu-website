40472b1df93958cf5c73c90df5a5a48398875b6f
Azzapop <a.b.hook@live.com.au>
2016-10-05 12:52:14 +1100

    split pie chart into two

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -6,7 +6,7 @@ class AdminDashboardController < ApplicationController
   def index
     this_month = Intake.all.order(start: :asc).reject{ |i| i.start.month != DateTime.now.month }
     this_month.map! {|i| i.course_name+','+i.start_date }
-    next_month = Intake.all.reject{ |i| i.start.month != (DateTime.now + 1.month).month }.sort{|x, y| x <=> y}
+    next_month = Intake.all.order(start: :asc).reject{ |i| i.start.month != (DateTime.now + 1.month).month }
     next_month.map! {|i| i.course_name+','+i.start_date }
     @upcoming_intakes = this_month.zip(next_month)
     bookings = Booking.where("created_at >= ?", Date.today-30.days)
@@ -14,6 +14,7 @@ class AdminDashboardController < ApplicationController
     bookings.each {|b| @booking_count[b.created_at.strftime("%b %d %Y")]+=1}
     @booking_count = @booking_count.inject([]) {|arr, v| arr << v}
     puts @booking_count.inspect
-    @pie_bookings = bookings.inject(Hash.new(0)) { |hash, i|  hash[i.course_name]+=1; hash }
+    @syd_pie_bookings = bookings.inject(Hash.new(0)) { |hash, i|  hash[i.course_name]+=1 if i.location.inlcude?("sydeny"); hash }
+    @mel_pie_bookings = bookings.inject(Hash.new(0)) { |hash, i|  hash[i.course_name]+=1 if i.location.inlcude?("melbourne"); hash }
   end
 end
