33e6003ff441a0c1dc9722ed072d48c8fb4b1c58
Azzapop <a.b.hook@live.com.au>
2016-10-05 11:52:44 +1100

    changes to dashboard page

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -4,14 +4,15 @@ class AdminDashboardController < ApplicationController
   layout "admin"
 
   def index
-    this_month = Intake.all.reject{ |i| i.start.month != DateTime.now.month }.sort{|x, y| x <=> y}
+    this_month = Intake.all.order(start: :asc).reject{ |i| i.start.month != DateTime.now.month }
     this_month.map! {|i| i.course_name+','+i.start_date }
-    next_month = Intake.all.reject{ |i| i.start.month != DateTime.now.month + 1.month }.sort{|x, y| x <=> y}
+    next_month = Intake.all.reject{ |i| i.start.month != (DateTime.now + 1.month).month }.sort{|x, y| x <=> y}
     next_month.map! {|i| i.course_name+','+i.start_date }
     @upcoming_intakes = this_month.zip(next_month)
     bookings = Booking.where("created_at >= ?", Date.today-30.days)
     @booking_count = Hash[(30.days.ago.to_date..Date.today).map{ |date| date.strftime("%b %d %Y") }.collect{|v| [v, 0]}]
     bookings.each {|b| @booking_count[b.created_at.strftime("%b %d %Y")]+=1}
+    puts @booking_count.inspect
     @pie_bookings = bookings.inject(Hash.new(0)) { |hash, i|  hash[i.course_name]+=1; hash }
   end
 end
