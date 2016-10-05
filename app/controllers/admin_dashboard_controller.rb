cf8d1b69a76c2148140f2ef34c01790e0ef45aee
Azzapop <a.b.hook@live.com.au>
2016-10-05 13:07:14 +1100

    show total count

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -12,7 +12,8 @@ class AdminDashboardController < ApplicationController
     bookings = Booking.where("created_at >= ?", Date.today-30.days)
     @booking_count = Hash[(30.days.ago.to_date..Date.today).map{ |date| date.strftime("%b %d %Y") }.collect{|v| [v, 0]}]
     bookings.each {|b| @booking_count[b.created_at.strftime("%b %d %Y")]+=1}
-    @booking_count = @booking_count.inject([]) {|arr, v| arr << v}
+    @booking_hash = @booking_count.inject([]) {|arr, v| arr << v}
+    @booking_count = bookings.count
     puts @booking_count.inspect
     @syd_pie_bookings = bookings.inject(Hash.new(0)) { |hash, i|  hash[i.course_name]+=1 if i.course_location.include?("Sydney"); hash }
     @mel_pie_bookings = bookings.inject(Hash.new(0)) { |hash, i|  hash[i.course_name]+=1 if i.course_location.include?("Melbourne"); hash }
