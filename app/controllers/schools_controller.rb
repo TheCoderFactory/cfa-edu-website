4f93b24eea0425d44a0dbdb697179e53351193b5
Azzapop <a.b.hook@live.com.au>
2016-11-03 16:50:43 +1100

    fix minor bugs

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -2,6 +2,6 @@ class SchoolsController < ApplicationController
   def index
     @coding_curriculum_courses = Course.where(subtype: "Coding Curriculum")
     @maker_space_courses = Course.where(subtype: "Maker Space")
-    @teacher_training_courses = Course.where(subtype: "Maker Space")
+    @teacher_training_courses = Course.where(subtype: "Teacher Training")
   end
 end
