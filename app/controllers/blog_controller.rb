30abc08ff9a675f2743b3b889d4b14729646c759
pedrogrande <pete@coderfactory.com>
2016-11-13 02:19:43 +1100

    added blog feed

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,10 +1,5 @@
 class BlogController < ApplicationController
   def index
     @posts = Post.all.current_articles.paginate(page: params[:page], per_page: 10)
-    @blog_feed = Post.current_articles
-    respond_to do |format|
-        format.html
-        format.atom
-      end
   end
 end
