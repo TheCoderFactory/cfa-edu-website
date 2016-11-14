d80613ff63922c060dc5072047713e8cea3a38a0
Azzapop <a.b.hook@live.com.au>
2016-11-15 10:22:35 +1100

    add tabs for school holidays section

--- app/assets/javascripts/kids-coding.js
+++ app/assets/javascripts/kids-coding.js
@@ -17,4 +17,22 @@ $(document).ready(function() {
     weekend_sec_btn.toggleClass("active");
     weekend_prim_btn.toggleClass("active");
   });
+  var sh_prim = $('.school-h-primary-content');
+  var sh_sec = $('.school-h-secondary-content');
+  sh_sec.hide();
+  var sh_prim_btn = $('#school-h-prim-btn');
+  sh_prim_btn.addClass("active");
+  var sh_sec_btn = $('#school-h-sec-btn');
+  $(sh_prim_btn).on("click", function() {
+    sh_sec.hide();
+    sh_prim.fadeIn("slow");
+    sh_sec_btn.toggleClass("active");
+    sh_prim_btn.toggleClass("active");
+  });
+  $(sh_sec_btn).on("click", function() {
+    sh_prim.hide();
+    sh_sec.fadeIn("slow");
+    sh_sec_btn.toggleClass("active");
+    sh_prim_btn.toggleClass("active");
+  });
 });
--- app/controllers/kids_coding_controller.rb
+++ app/controllers/kids_coding_controller.rb
@@ -1,8 +1,10 @@
 class KidsCodingController < ApplicationController
   def index
     @weekend_courses = Course.where(course_type: "Kids").where("subtype like ?", "%Weekend%")
-    @weekend_7_12_courses = @weekend_courses.where(subtype: "Weekend 7-12")
-    @weekend_12_plus_courses = @weekend_courses.where(subtype: "Weekend 12+")
-    @school_holiday_courses = Course.where(subtype: "School Holidays")
+    @weekend_primary_courses = @weekend_courses.where(subtype: "Weekend Primary")
+    @weekend_secondary_courses = @weekend_courses.where(subtype: "Weekend Secondary")
+    @sh_courses = Course.where(course_type: "Kids").where("subtype like ?", "%School Holidays%")
+    @sh_primary_courses = @sh_courses.where(subtype: "School Holidays Primary")
+    @sh_secondary_courses = @sh_courses.where(subtype: "School Holidays Secondary")
   end
 end
