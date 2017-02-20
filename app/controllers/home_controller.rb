b6b5c3c7aa8418ada1bfdf7ffa1e39574d2e0b91
Azzapop <a.b.hook@live.com.au>
2017-02-20 16:56:44 +1100

    remove intake hack

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @intakes = Intake.all.future_intakes.active_intakes.chron_order
-    @intakes = @intakes.group("course_id", "location").having("start = MIN(start)").first(4)
+    @intakes = Intake.all.future_intakes.active_intakes.chron_order.first(4)
     @posts = Post.current_articles.first(3)
   end
 end
